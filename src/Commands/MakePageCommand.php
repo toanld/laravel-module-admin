11f2aa2fda1e66879ca92f3d48b3125edb9977ad
HungNM <hungnm1@vatgia.com>
2022-03-21 00:05:29 +0700

    fix folder

--- src/Commands/MakePageCommand.php
+++ src/Commands/MakePageCommand.php
@@ -3,6 +3,7 @@
 namespace Hungnm28\LaravelModuleAdmin\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 
 class MakePageCommand extends Command
@@ -16,6 +17,21 @@ class MakePageCommand extends Command
     {
         $name = $this->argument('name');
         $modelName = $this->option("model");
+        $name = $this->argument('name');
+        $arr = explode("/",$name);
+        foreach($arr as $k=>$n){
+            $arr[$k] = Str::studly(Str::plural($n));
+
+        }
+        $name = implode("/",$arr);
+        $modelName = $this->option("model");
+        $this->folder = $name;
+        if (!$modelName) {
+            $modelName = Str::singular($name);
+            $modelName = Str::studly($modelName);
+        }
+        $this->model = $modelName;
+
         $this->info($this->description);
         $this->call('lma:make-list', ['name' => $name, '--model' => $modelName]);
         $this->call('lma:make-create', ['name' => $name, '--model' => $modelName]);
