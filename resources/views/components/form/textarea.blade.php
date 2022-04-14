8284be1b748020d1223653438b6d5fa156acca64
HungNM <hungnm1@vatgia.com>
2022-04-14 11:00:51 +0700

    add force to make-page

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -5,6 +5,6 @@
 
 @endphp
 <x-lma.form.field :name="$name" :label="$label">
-    <textarea wire:model{{$mode}}="{{$name}}" type="{{$type}}" placeholder="{{$placeholder}}"
+    <textarea wire:model{{$mode}}="{{$name}}" placeholder="{{$placeholder}}"
              {{$attributes}} rows="{{$rows}}" class="w-full  p-1 px-2 text-sm @error($name) border-orange-500 text-orange-500 @enderror"></textarea>
 </x-lma.form.field>
