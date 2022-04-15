7f8027bf774c6b79d046455cc202254006751878
HungNM <hungnm1@vatgia.com>
2022-04-15 11:05:18 +0700

    update create menu

--- publishes/resources/views/livewire/auth/admin-menus/create.blade.php
+++ publishes/resources/views/livewire/auth/admin-menus/create.blade.php
@@ -1,7 +1,8 @@
 <div class="w-full min-h-screen">
     <x-lma.header-page group="Auth/ Admin Menus" page="Create" />
     <x-lma.box.create listing="{{route('admin.auth.admin-menus')}}" >
-        <x-lma.form.input type="text" name="route" label="Route" />
+        <x-lma.form.select  name="route" label="Select Route" :params="$routes" />
+        <x-lma.form.input  name="route" label="Or Insert Route"  />
         <x-lma.form.icon name="icon" label="Icon" :val="$icon"/>
         <x-lma.form.input type="text" name="name" label="Name" />
         <x-lma.form.select  name="parent_id" label="Parent id" :params="$parents" :default="['ROOT']" />
