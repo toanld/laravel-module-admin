904c1d28924e10f9689d4d19904df5c1e2b46410
HungNM <hungnm1@vatgia.com>
2022-04-17 11:15:19 +0700

    add upload url for tinymce

--- resources/views/components/form/tinymce.blade.php
+++ resources/views/components/form/tinymce.blade.php
@@ -1,4 +1,4 @@
-@props(["name","label"=>null,'placeholder'=>null,'rows'=>5,'plugins'=>'code table lists image fullscreen','toolbar'=>'blocks | bold italic | alignleft aligncenter alignright | image | indent outdent | bullist numlist | code | table | fullscreen'])
+@props(["name","label"=>null,'placeholder'=>null,'rows'=>5,'upload_url'=>'','plugins'=>'code table lists image fullscreen','toolbar'=>'blocks | bold italic | alignleft aligncenter alignright | image | indent outdent | bullist numlist | code | table | fullscreen'])
 @php
     $placeholder = ($placeholder!=''?$placeholder:Illuminate\Support\Str::headline($name)).'...';
 
@@ -18,6 +18,7 @@
                          selector: '#{{$name}}',
                          plugins: '{{$plugins}}',
                          toolbar: '{{$toolbar}}',
+                         {{$upload_url?"images_upload_url:'".$upload_url."',":''}}
                           setup: function (editor) {
                              editor.on('init change', function () {
                                        editor.save();
