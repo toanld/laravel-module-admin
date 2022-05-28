c3f05568087c457b1abec5f8da7130cd555fa04e
HungNM <hungnm1@vatgia.com>
2022-05-28 22:53:44 +0700

    add form checkbox

---
+++ resources/views/components/form/checkbox.blade.php
@@ -0,0 +1,12 @@
+@props(["name","label"=>null,"title"=>null, "class"=>"",'placeholder'=>null, 'value'=>1])
+@php
+    $placeholder = ($placeholder!=''?$placeholder:Illuminate\Support\Str::headline($name)).'...';
+@endphp
+<x-lma.form.field :name="$name" :label="$label">
+    <div class="w-full flex flex-wrap space-x-2 md:pt-2">
+        <label class="flex-none flex items-center">
+            <input type="checkbox" wire:model="{{$name}}" value="{{$value}}">
+            <span class="flex-auto pl-1 pr-4">{{$title}}</span>
+        </label>
+    </div>
+</x-lma.form.field>
