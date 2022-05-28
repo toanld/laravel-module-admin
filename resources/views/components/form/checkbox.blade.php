734c221ba660dbf2e19d18c7e543a3e04124154e
HungNM <hungnm1@vatgia.com>
2022-05-28 23:04:19 +0700

    fix checkbox, radio

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -3,7 +3,7 @@
     $placeholder = ($placeholder!=''?$placeholder:Illuminate\Support\Str::headline($name)).'...';
 @endphp
 <x-lma.form.field :name="$name" :label="$label">
-    <div class="w-full flex flex-wrap space-x-2 md:pt-2">
+    <div class="w-full flex flex-wrap space-x-2 md:pt-1">
         <label class="flex-none flex items-center">
             <input type="checkbox" wire:model="{{$name}}" value="{{$value}}">
             <span class="flex-auto pl-1 pr-4">{{$title}}</span>
--- resources/views/components/form/radio.blade.php
+++ resources/views/components/form/radio.blade.php
@@ -3,7 +3,7 @@
     $placeholder = ($placeholder!=''?$placeholder:Illuminate\Support\Str::headline($name)).'...';
 @endphp
 <x-lma.form.field :name="$name" :label="$label">
-    <div class="w-full flex flex-wrap space-x-2 md:pt-2">
+    <div class="w-full flex flex-wrap space-x-2 md:pt-1">
         @foreach($params as $k=>$val)
             <label class="flex-none flex items-center">
                 <input type="radio" wire:model="{{$name}}" value="{{$k}}">
